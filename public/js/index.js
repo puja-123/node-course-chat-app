7f8763d6989245068fc169f8ccbf0d677861459d
puja-123 <bhard.puja@gmail.com>
2018-11-09 01:10:59 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -25,12 +25,13 @@
 });
 jQuery('#message-form').on('submit', function(e) {
         e.preventDefault();
+     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
           from: 'User',
-          text: jQuery('[name=message]').val()
+          text: messageTextbox.val()
    }, function () {
-
+         messageTextbox.val('')
    });
 });
 
@@ -39,13 +40,16 @@ jQuery('#message-form').on('submit', function(e) {
      if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
       }
+locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
-    
+      locationButton.removeAttr('disabled').text('Send location') ;   
       socket.emit('createLocationMessage', {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
       });
   }, function () {
+     locationButton.removeAttr('disabled').text('Send location'); 
      alert('Unable to fetch location.');
     });
 });
--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(publicPath));
    socket.on('createMessage', (message , callback) => {
       console.log('createMessage', message);
             io.emit('newMessage',generateMessage( message.from,message.text));
-   callback('this is from server');
+   callback();
   });
  socket.on('createLocationMessage', (coords) => {
  io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude ));
