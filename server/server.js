2391b646c8e3b0864c1e07525b6a94a29ae45552
puja-123 <bhard.puja@gmail.com>
2018-11-12 03:42:17 +0530

    Send message to only people in same room

--- server/server.js
+++ server/server.js
@@ -32,17 +32,24 @@ app.use(express.static(publicPath));
      io.to(params.room).emit('updateUserList', users.getUserList(params.room));
      socket.emit('newMessage', generateMessage('Admin','Welcome to the chat app'));
    
-   socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} joined.`));
+   socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
       callback();
 });
    
    socket.on('createMessage', (message , callback) => {
-      console.log('createMessage', message);
-            io.emit('newMessage',generateMessage( message.from,message.text));
+      var user = users.getUser(socket.id);
+
+    if(user && isRealString(message.text)) 
+{
+   io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+}
    callback();
   });
  socket.on('createLocationMessage', (coords) => {
- io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude ));
+    var user = users.getUser(socket.id);
+  if(user) {
+ io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude ));
+}
  });  
 
    socket.on('disconnect', () =>{
